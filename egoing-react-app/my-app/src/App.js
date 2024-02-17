b24431a5d3616c9d418329677f13adf1312ab137
shkim9783 <shkim9783@gmail.com>
2024-02-17 16:38:19 +0900

    20240417 study

--- egoing-react-app/my-app/src/App.js
+++ egoing-react-app/my-app/src/App.js
@@ -1,18 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+/* 
+// 구문오류 발생
+// 해당함수 사용하려면 javascript:return false; 사용해야하는데 리액트에서는 권장되지 않음
+function OnChangeMode(event){
+  // event 객체를 못 찾아오는듯
+  console.log(event);
+  // event.preventDefault();
+  alert("Header!");
+}  
+*/
+
 function Header(props) {
   return (
-    <h1>
-      <a href={props.href}>{props.title}</a>
-    </h1>
+    <header>
+      <h1>
+        <a href={props.href} onClick={(event)=>{
+            event.preventDefault();
+            // 상위에서 정의된 함수를 내부에서 실행한다.
+            props.onChangeMode();
+        }}>{props.title}</a>
+      </h1>
+    </header>
   );
 }
 
 function Nav(props) {
   const navList = props.topices.map((list, index) => (
     <li key={index}>
-      <a herf="">{list.title}</a>
+      <a href={'/read/'+index} onClick={(event)=>{
+            event.preventDefault();
+            //props.onChangeMode();
+            alert(list.title);
+      }}>{list.title}</a>
     </li>
   ));
 
@@ -32,6 +53,11 @@ function Section(props) {
   );
 }
 
+
+
+
+
+
 function App() {
   const topices = [
     { title: "html", content: "html is..." },
@@ -41,9 +67,13 @@ function App() {
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
-      <Header title="WEB" href="/"></Header>
+      <Header title="WEB" href="/" onChangeMode={()=>{
+        alert("Header!");
+      }}></Header>
 
-      <Nav topices={topices}></Nav>
+      <Nav topices={topices} onChangeMode={()=>{
+        //alert(topices);
+      }}></Nav>
 
       <Section title="Welcome" content="Hello, WEB"></Section>
     </div>
